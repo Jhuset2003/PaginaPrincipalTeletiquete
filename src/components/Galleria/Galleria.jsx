7af13ac486d57f76aa2b4124dff838f5ad9c4de6
jhuset2003 <jhusetqp@gmail.com>
2022-10-24 10:03:41 -0500

    Landing v1.4

--- src/components/Galleria/Galleria.jsx
+++ src/components/Galleria/Galleria.jsx
@@ -1,17 +1,67 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
 import { InfoGalleria } from './InfoGalleria'
+import GalleriaStyles from "../../styles/componentsStyles/Galleria/Galleria.module.css"
+import { BsArrowRight } from 'react-icons/bs';
 
 
 export const Galleria = () => {
+  const [first,setFirt] = useState([]);
+  const [All,setAll] = useState([]);
+
+  function guardaTodo(Data) {
+    setAll(Data)
+  }
+
+  useEffect (()=> {
+    function takeOne() {
+      var information = []
+        InfoGalleria.map((info,index)=> {
+          
+          if (index === 0) {
+            setFirt(info)
+          }
+          else {
+            information.push(info)
+            guardaTodo(information)
+          }
+
+        })
+    
+    }
+    takeOne()
+   },[first])
+  
+
   return (
-    <>
-        {InfoGalleria.map((Info,index)=>{
+    <div className={GalleriaStyles.GalleriaContainer}>
+      <div className={GalleriaStyles.imgsConatiner}>
+        <div className={GalleriaStyles.imgs}>
+          <div className={GalleriaStyles.firstImageContainer}>
+            <div className={GalleriaStyles.firstImage}>
+            <img src={first.imgUrl} alt={first.ciudad} />
+              <div>
+                <p>{first.ciudad}</p>
+                <button><BsArrowRight/></button>
+              </div>
+            </div>
+          </div>
+
+         <div className={GalleriaStyles.OtherImagesContainer}>
+          {All.map((data,index)=>{
             return(
-                <div key={index}>
-                    <img src={Info.imgUrl} alt="" srcset="" key={index}/>   
+              <div className={GalleriaStyles.OtherImages} key={index}>
+                <img src={data.imgUrl} alt={data.ciudad} />
+                <div>
+                  <p>{data.ciudad}</p>
+                  <button><BsArrowRight/></button>
                 </div>
+              </div>
             )
-        })}    
-    </>
+          })}
+         </div>
+        </div>
+      </div>
+     
+    </div>
   )
 }
--- src/components/Galleria/InfoGalleria.jsx
+++ src/components/Galleria/InfoGalleria.jsx
@@ -1,7 +1,22 @@
 export const InfoGalleria = [
-    {imgUrl: "https://cdn.pixabay.com/photo/2022/10/16/13/53/early-morning-7525151_960_720.jpg"},
-    {imgUrl: "https://cdn.pixabay.com/photo/2022/09/02/07/31/alps-7426887_960_720.jpg"},
-    {imgUrl: "https://cdn.pixabay.com/photo/2022/10/14/06/19/man-7520571_960_720.jpg"},
-    {imgUrl: "https://cdn.pixabay.com/photo/2022/10/14/07/12/muslim-7520627_960_720.jpg"},
-    {imgUrl: "https://cdn.pixabay.com/photo/2022/09/24/16/32/bulldog-7476727_960_720.jpg"},
+    {
+        imgUrl: "https://cdn.pixabay.com/photo/2022/10/16/13/53/early-morning-7525151_960_720.jpg",
+        ciudad:"Cali"
+    },
+    {
+        imgUrl: "https://cdn.pixabay.com/photo/2022/09/02/07/31/alps-7426887_960_720.jpg",
+        ciudad:"Cartagena"
+    },
+    {
+        imgUrl: "https://cdn.pixabay.com/photo/2022/10/14/06/19/man-7520571_960_720.jpg",
+        ciudad:"Medellin"
+    },
+    {
+        imgUrl: "https://cdn.pixabay.com/photo/2022/10/14/07/12/muslim-7520627_960_720.jpg",
+        ciudad:"Bucaramanga"
+    },
+    {
+        imgUrl: "https://cdn.pixabay.com/photo/2022/09/24/16/32/bulldog-7476727_960_720.jpg",
+        ciudad:"Santa fe"
+    },
 ]
